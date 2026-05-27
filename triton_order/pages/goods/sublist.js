fd21d43f652a3cdf328171b95ee90cabddb4738a
Your Name <you@example.com>
2026-05-27 20:38:53 +0800

    shop goods

--- triton_order/pages/goods/sublist.js
+++ triton_order/pages/goods/sublist.js
@@ -1,16 +1,12 @@
 const TOOLS = require('../../utils/tools.js')
-const AUTH = require('../../utils/auth')
 const APP = getApp()
 
 const CLOUDFUNC = require('../../utils/cloud.js');
 
 Page({
   data: {
-    userName:'',
-    userID:-1,
-    bankerID:-1,
-    goodsRecommend: [],
-    bankers: [],
+    bankerName: '',
+    bankerID: -1,
     goods: [],
     loadingMoreHidden: true,
     curPage: 1,
@@ -24,23 +20,11 @@ Page({
   },
   onLoad: function(options) {
     const bankerID = options.banker_id ? Number(options.banker_id) : -1;
-    console.log(options)
-    this.setData({bankerID:bankerID})
+    this.setData({ bankerID: bankerID })
     wx.showShareMenu({
       withShareTicket: true,
     })
-    this.getGoodsList(false, bankerID)
-  },
-  readConfigVal() {
-    const userName = wx.getStorageSync('userName')
-    const userID = wx.getStorageSync('userName')
-    wx.setNavigationBarTitle({
-      title: userName
-    })
-    this.setData({
-      userID  : this.userID   ? this.userID   : -1,
-      userName: userName ? userName : 'who are you',
-    })
+    this.getGoodsList(false)
   },
   onShow: function(e){
     this.setData({
@@ -51,41 +35,29 @@ Page({
     })
     TOOLS.showTabBarBadge()
     this.setData({ curPage: 1 })
-    this.getGoodsList(false, this.data.bankerID)
+    this.getGoodsList(false)
   },
 
-  async getGoodsList(append, bankerID) {
-    wx.showLoading({
-      title: ''
-    })
-    const res = await CLOUDFUNC.callCloudFunction('goodsStatics', { bankerID: this.data.bankerID, pageNo: this.data.curPage, pageSize: this.data.pageSize });
+  async getGoodsList(append) {
+    wx.showLoading({ title: '' })
+    const res = await CLOUDFUNC.callCloudFunction('goodsStatics', { userID: this.data.bankerID, pageNo: this.data.curPage, pageSize: this.data.pageSize });
     wx.hideLoading()
     if (res.code != 0) {
-      let newData = {
-        loadingMoreHidden: false
-      }
-      if (!append) {
-        newData.goods = []
-      }
+      let newData = { loadingMoreHidden: false }
+      if (!append) { newData.goods = [] }
       this.setData(newData);
       return
     }
     let goods = [];
-    if (append) {
-      goods = this.data.goods
-    }
+    if (append) { goods = this.data.goods }
     for (var i = 0; i < res.goods.length; i++) {
       const item = res.goods[i];
-      // 转换数据结构以适配前端显示
       goods.push({
         id: item.goods_id,
         name: item.goods_info?.name || '',
         price: item.goods_info?.price || '0',
-        pic: (item.goods_info?.imageList && item.goods_info.imageList[0]) ? item.goods_info.imageList[0].url : '',
-        color: item.goods_info?.color || '',
-        sizeX: item.goods_info?.sizeX || '',
-        sizeY: item.goods_info?.sizeY || '',
-        sizeZ: item.goods_info?.sizeZ || ''
+        characteristic: item.goods_info?.characteristic || '',
+        pic: (item.goods_info?.imageList && item.goods_info.imageList[0]) ? item.goods_info.imageList[0].url : ''
       });
     }
     this.setData({
@@ -99,7 +71,7 @@ Page({
       path: '/pages/index/index?inviter_id=' + wx.getStorageSync('userID')
     }
   },
-  onShareTimeline() {    
+  onShareTimeline() {
     return {
       title: '"' + wx.getStorageSync('userName') + '" ' + wx.getStorageSync('share_profile'),
       query: 'inviter_id=' + wx.getStorageSync('userID'),
@@ -107,16 +79,12 @@ Page({
     }
   },
   onReachBottom: function() {
-    this.setData({
-      curPage: this.data.curPage + 1
-    });
-    this.getGoodsList(true, this.data.bankerID)
+    this.setData({ curPage: this.data.curPage + 1 });
+    this.getGoodsList(true)
   },
   onPullDownRefresh: function() {
-    this.setData({
-      curPage: 1
-    });
-    this.getGoodsList(true, this.data.bankerID)
+    this.setData({ curPage: 1 });
+    this.getGoodsList(false)
     wx.stopPullDownRefresh()
   }
 })
